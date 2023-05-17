585868c5079182e268d5e7b7f461231f59fc523c
Mohammed Husamuddin <uni.husam@gmail.com>
2023-05-17 15:10:44 +0400

    Full Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,36 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 
+st.set_page_config(page_title="University Dashboard")
+
+
 def main():
+    st.set_page_config(layout="wide")
+
     st.title("University Clubs Dashboard")
     st.markdown("---")
 
+    # Sidebar
+    st.sidebar.title("Navigation")
+    section = st.sidebar.selectbox("Go to", ("Home", "Clubs Resources", "University Resources"))
+
+    if section == "Home":
+        show_homepage()
+    elif section == "Clubs Resources":
+        show_clubs_resources()
+    elif section == "University Resources":
+        show_university_resources()
+
+    st.divider()
+    st.caption("Designed & Developed by HUSAM")
+
+
+def show_homepage():
+    st.header("Welcome to the University Clubs Dashboard!")
+    st.write("Use the sidebar to navigate to different sections.")
+
+
+def show_clubs_resources():
     st.header("Clubs Resources")
     st.markdown("---")
 
@@ -25,7 +51,8 @@ def main():
                 '<iframe src="https://uni-notes.netlify.app/" width="800" height="400"></iframe>'
                 '</div>', unsafe_allow_html=True)
 
-    st.markdown("---")
+
+def show_university_resources():
     st.header("University Resources")
     st.markdown("---")
 
@@ -46,8 +73,6 @@ def main():
                 '<iframe src="https://erp.bits-pilani.ac.in/" width="1200" height="400"></iframe>'
                 '</div>', unsafe_allow_html=True)
 
+
 if __name__ == '__main__':
     main()
-st.divider()
-st.caption("Designed & Developed by HUSAM")
-
