a3bd569a11defaf422eb421908884e7b889c29ac
Mohammed Husamuddin <uni.husam@gmail.com>
2023-09-30 00:43:35 +0400

    new look

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import pandas as pd
 
 def main():
-    st.set_page_config(page_title="unidash", layout="centered")
-    st.title("University Student Dashboard")
+    st.title("University Clubs Dashboard")
     st.markdown("---")
 
     # Sidebar
     st.sidebar.title("Navigation")
-    section = st.sidebar.selectbox("Go to", ("Home", "Clubs Resources", "University Resources", "Profile"))
+    section = st.sidebar.selectbox("Go to", ("Home", "Clubs Resources", "University Resources"))
 
     if section == "Home":
         show_homepage()
@@ -16,44 +14,44 @@ def main():
         show_clubs_resources()
     elif section == "University Resources":
         show_university_resources()
-    elif section == "Profile":
-        show_profile()
 
     st.divider()
-    st.caption("Designed & Developed by Mohammed Husamuddin")
+    st.caption("Designed & Developed by HUSAM")
     st.caption("The app is designed using Streamlit")
 
 def show_homepage():
-    st.header("Welcome to the University Student Dashboard!")
+    st.header("Welcome to the University Clubs Dashboard!")
     st.write("Use the sidebar to navigate to different sections.")
 
 def show_clubs_resources():
     st.header("Clubs Resources")
     st.markdown("---")
 
-    # ... Your club resources content ...
+    st.subheader("GDSC Resources")
+    st.markdown('<iframe src="https://gdscbpdc.github.io/" width="800" height="400"></iframe>', unsafe_allow_html=True)
+
+    st.markdown("---")
+    st.subheader("ACM Resources")
+    st.markdown('<iframe src="https://openlib-cs.acmbpdc.org/" width="800" height="400"></iframe>', unsafe_allow_html=True)
+
+    st.markdown("---")
+    st.subheader("Ahmed Thahir's Notes")
+    st.markdown('<iframe src="https://uni-notes.netlify.app/" width="800" height="400"></iframe>', unsafe_allow_html=True)
 
 def show_university_resources():
     st.header("University Resources")
     st.markdown("---")
 
-    # ... Your university resources content ...
+    st.subheader("Library Resources")
+    st.markdown('<iframe src="http://webopac.bits-dubai.ac.ae/AutoLib/index.jsp" width="800" height="400"></iframe>', unsafe_allow_html=True)
 
-def show_profile():
-    st.header("User Profile")
     st.markdown("---")
+    st.subheader("Courses & LMS")
+    st.markdown('<iframe src="https://lms.bitspilanidubai.ae/login/index.php" width="800" height="400"></iframe>', unsafe_allow_html=True)
 
-    st.sidebar.subheader("Login")
-    username = st.sidebar.text_input("Username")
-    password = st.sidebar.text_input("Password", type="password")
-    if st.sidebar.button("Login"):
-        if authenticate(username, password):
-            user_info = get_user_data(username)
-            st.write(f"Welcome, {user_info.get('name', 'User')}!")
-            st.write(f"Enrolled Courses: {', '.join(user_info.get('courses', []))}")
-            st.write(f"Bookmarks: {', '.join(user_info.get('bookmarks', []))}")
-        else:
-            st.warning("Invalid username or password.")
+    st.markdown("---")
+    st.subheader("BITS ERP")
+    st.markdown('<iframe src="https://erp.bits-pilani.ac.in/" width="800" height="400"></iframe>', unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
